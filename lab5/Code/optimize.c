e9899a451b3ad544707b7495e8da00a57dfa4bc2
quanhaoli <quanhaoli@quanhaoli-Ubuntu20.04>
2022-11-24 20:55:19 +0800

    eliminate common sub_exp finish

--- lab5/Code/optimize.c
+++ lab5/Code/optimize.c
@@ -4,6 +4,10 @@ Node SSA_head = NULL; //存放所有的SSA形式的中间代码
 int V[1024] = { 0 }; //记录从三地址码转化为SSA的过程中vi变量对应的t_id
 int T[1024] = { 0 }; //记录从三地址码转化为SSA的过程中ti变量对应的t_id
 int cur_id = 0; //记录当前SSA转换过程中记录到t_id
+struct T_state states[1023] = {0}; //记录当前的SSA中每个t_id的状态
+struct exp_ sub_exp[1023] = {0}; //存放子表达式的表
+int sub_exp_num = 0;//当前存了几个子表达式
+
 Node newNode(char* buf)
 {
     Node p = (Node)(malloc(sizeof(struct Node_)));
@@ -42,9 +46,9 @@ void delete_node(Node p) //将一句SSA代码删除
         p1 = p1->next;
     }
     p1->next = p->next;
-    //free(p); 不确定会不会出问题
     return;
 }
+
 char t_name[1023] = {"t"};
 char* trans_variable(char* name, int flag) //把变量变为t_id; 如果是常量就不变; flag表示是否要把cur_id加1
 {
@@ -214,7 +218,38 @@ void printSSA(FILE* file)
     return;
 }
 
-/*void remove_common_subexp() //消除公共子表达式
+char* trans_T(char* operand) //将t_id转化为t_referid
+{
+    assert(operand[0] == '#' || operand[0] == 't');
+    if(operand[0] == '#')// 返回常数
+    {
+        //do nothing
+        return operand;
+    }
+    else
+    {
+        int id = atoi(operand + 1);
+        if(states[id].is_dead == 0)
+        {
+            // do nothing
+            return operand;
+        }
+        else
+        {
+            int refer_id = states[id].refer_id;
+            char num[10];
+            sprintf(num, "%d", refer_id);
+            strcpy(t_name, "t");
+            strcat(t_name, num);
+            strcpy(operand, t_name);
+            return operand;
+        }
+    }
+    assert(0);
+    return NULL;
+}
+
+void remove_common_subexp() //消除公共子表达式
 {
     Node p = SSA_head;
     while(p != NULL)
@@ -232,31 +267,109 @@ void printSSA(FILE* file)
             //operand从7--strlen(exp)-1
             char operand[1023] = {""};
             strcpy(operand, p->exp+7);
-            assert(operand[0] == '#' || operand[0] == 't' || operand[0] == 'v');
-            if(operand[0] == '#')// 返回常数
-            {
-                //do nothing
-            }
-            else if(operand[0] == 't')
-            {
-
-            }
-            else
-            {
-
-            }
+            strcpy(operand, trans_T(operand));
+            char return_[1023] = {"RETURN "};
+            strcat(return_, operand);
+            strcpy(p->exp, return_);
         }
         else if(p->exp[0] == 'W') //WRITE
         {
-
+            //operand从6--strlen(exp)-1
+            char operand[1023] = {""};
+            strcpy(operand, p->exp+6);
+            strcpy(operand, trans_T(operand));
+            char write_[1023] = {"WRITE "};
+            strcat(write_, operand);
+            strcpy(p->exp, write_);
         }
         else //赋值语句
         {
+            int colon_pos = -1; //冒号的位置
+            for (int i = 0; i < strlen(p->exp); i++)
+            {
+                if (p->exp[i] == ':')
+                {
+                    colon_pos = i;
+                    break;
+                }
+            }
+            assert(colon_pos != -1);
+            //从0--colon_pos-2是左边的变量；从colon_pos+3--strlen(exp)-1是右边的表达式
+            char left[1023] = { "" };
+            char right[1023] = { "" };
+            strncpy(left, p->exp, colon_pos - 1);
+            strncpy(right, p->exp + colon_pos + 3, (strlen(p->exp) - 1) - (colon_pos + 3) + 1);
 
+            //对left不用操作, 提取出operand_id
+            int left_id = atoi(left + 1);
+            //对right检查是否是公共子表达式
+            int found = 0; //是否找到重复的子表达式
+            for(int i=0;i<sub_exp_num;i++)
+            {
+                if(strcmp(sub_exp[i].exp, right) == 0) //是公共子表达式
+                {
+                    found = 1;
+                    //更新states
+                    states[left_id].is_dead = 1;
+                    states[left_id].refer_id = sub_exp[i].refer_id;
+                    //删除这行SSA中间代码
+                    delete_node(p);
+                    break;
+                }
+            }
+            if(found == 0)
+            {
+                
+                //把这个表达式信息添加到子表达式表中
+                strcpy(sub_exp[sub_exp_num].exp, right);
+                sub_exp[sub_exp_num].refer_id = left_id;
+                sub_exp_num++;
+                //检查right,看是否有变量需要替换成refer_id
+                int op_pos = -1;
+                for (int i = 0; i < strlen(right); i++)
+                {
+                    if (right[i] == '+' || right[i] == '-' || right[i] == '*' || right[i] == '/')
+                    {
+                        op_pos = i;
+                        break;
+                    }
+                }
+                if (op_pos == -1) //说明是一个赋值语句
+                {
+                    //整个right就是右边的operand
+                    strcpy(right, trans_T(right));
+                }
+                else
+                {
+                    //从0--op_pos-2是operand1;从op_pos+2--strlen(right)-1是operand2
+                    char operand1[1023] = { "" };
+                    char operand2[1023] = { "" };
+                    strncpy(operand1, right, op_pos - 1);
+                    strncpy(operand2, right + op_pos + 2, (strlen(right) - 1) - (op_pos + 2) + 1);
+                    strcpy(operand1, trans_T(operand1));
+                    strcpy(operand2, trans_T(operand2));
+                    char op = right[op_pos];
+                    char Z[100] = "   ";
+                    Z[1] = op;
+                    strcat(Z, operand2);
+                    strcat(operand1, Z);
+                    strcpy(right, operand1);
+                }
+                //现在right被更新完毕，更新p节点
+                char inter[1023] = { " := " };
+                strcat(inter, right);
+                strcat(left, inter);
+                strcpy(p->exp, left);
+            }
+            else
+            {
+                // do nothing
+            }
         }
+        assert(p != NULL);
         p = p->next;
     }
-}*/
+}
 
 void IR_optimize(FILE* file1, FILE* file2)
 {
@@ -274,7 +387,7 @@ void IR_optimize(FILE* file1, FILE* file2)
         }
         Trans2SSA(copy);
     }
-    //remove_common_subexp();
+    remove_common_subexp();
     printSSA(file2); //仅作调试
     return;
 }
--- lab5/Code/optimize.h
+++ lab5/Code/optimize.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +9,16 @@ struct Node_
     Node next;
 };//存放SSA指令的链表节点
 
+struct T_state
+{
+    int is_dead; //该t_id是否已经dead
+    int refer_id; //如果t_id已经dead，它的替代品的id
+};
 
+struct exp_
+{
+    char exp[1023]; //存放这个子表达式的等号右边
+    int refer_id; //这个子表达式的左边是t_referid
+};
 
 void IR_optimize(FILE* file1, FILE* file2);
